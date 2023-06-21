061d2f6a3e30a9c3bed30b6780abe7fec88891d0
Tien Nguyen <tiennguyen@Tiens-MacBook-Pro.local>
2023-06-21 14:54:23 -0700

    Created display for and states for caclculator.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import Button from "./components/Button"
-import { Container} from "react-bootstrap"
-import "./styles/App.css"
-
+import { useState } from "react";
+import Button from "./components/Button";
+import { Container, Row , Col} from "react-bootstrap";
+import "./styles/App.css";
 
 function App() {
+  const [curr, setCurr] = useState(0);
+  const [symbol, setSymbol] = useState("");
+  const [total, setTotal] = useState(0);
+
+
   return (
     <>
       <Container className="app-container">
+        <Row>
+          <Col className="display" xs={12}> {curr} </Col>
+        </Row>
         <Button />
       </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,35 @@
 import { Row, Col } from "react-bootstrap";
 
-
 export default function Button() {
-    const renderButtons = buttonArr.map((x, y) => {
-        return <Col className={x[1]} key={y} xs={x[0] === '0' ? 6 : 3}>{x[0]}</Col>
-    })
+  const renderButtons = buttonArr.map((x, y) => {
+    return (
+      <Col className={`${x[1]} button`} key={y} xs={x[0] === "0" ? 6 : 3}>
+        {x[0]}
+      </Col>
+    );
+  });
 
-    return(
-        <>
-            <Row>
-                {renderButtons}
-            </Row>
-        </>
-    )
+  return <Row>{renderButtons}</Row>;
 }
 
-const buttonArr = [
-    ["AC", "extra"], ["±", "extra"], ["%", "extra"], ["÷", "symbol"],
-    ["7", "num"], ["8", "num"], ["3", "num"], ["x", "symbol"],
-    ["4", "num"], ["5", "num"], ["6", "num"], ["-", "symbol"],
-    ["1", "num"], ["2", "num"], ["9", "num"], ["+", "symbol"],
-    ["0", "num"], [".", "num"], ["=", "symbol"] 
-]
+const buttonArr: string[][] = [
+  ["AC", "extra"],
+  ["±", "extra"],
+  ["%", "extra"],
+  ["÷", "symbol"],
+  ["7", "num"],
+  ["8", "num"],
+  ["3", "num"],
+  ["x", "symbol"],
+  ["4", "num"],
+  ["5", "num"],
+  ["6", "num"],
+  ["-", "symbol"],
+  ["1", "num"],
+  ["2", "num"],
+  ["9", "num"],
+  ["+", "symbol"],
+  ["0", "zero"],
+  [".", "num"],
+  ["=", "symbol"],
+];
